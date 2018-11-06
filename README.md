1c5afc5b2d2475398cd251efeec669621f368be4
hideokamoto <hide.okamoto@digitalcube.jp>
2018-11-06 11:45:36 +0900

    docs

---
+++ README.md
@@ -0,0 +1,1 @@
+# [WIP] Shifter SDK for Node.js
--- classes/status.js
+++ classes/status.js
@@ -14,7 +14,7 @@ class Status extends Base {
   /**
    * Get Shifter Status API
    * @see {@link https://apidoc.getshifter.io/#!/status/get_status}
-   * @return {Promise<{"status":"number","body":{"maintenance":{"message":"string","target":["string"],"status":"string"},"user":{"credit_card_status":"string","upper_limit_projects":"number","max_transfer_gb":"number","max_disk_gb":"number","domain_setting":"string","netlify_deploy":"string","import_archives":"string","export_archives":"string","backup":"number","enable_subuser":"string","notification":{"email":{"destination":"string"}},"group":{"name":"string"}}}}>} API response
+   * @return {Promise<{}>} API response
    * @example
    * const Shifter = require('./index')
    * const token = 'XXXXX'
