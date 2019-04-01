3c9355e3579835eab7a8ccf1be7d924e7b6505ed
hideokamoto <hide.okamoto@digitalcube.jp>
2019-04-01 19:24:30 +0900

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,21 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+// import nodeResolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
+// import json from 'rollup-plugin-json';
 import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
 export default {
   input: './libs/index.ts',
   output: [{
     file: './dist/index.js',
     format: 'cjs'
   }, {
-    file: './dist/index.umd.js',
+    file: './umd/index.js',
     format: 'umd',
     name: 'Shifter'
   }],
   plugins: [
-    nodeResolve(),
-    commonjs(),
-    json(),
+    // nodeResolve({jsnext: true}),
+    // commonjs(),
+    // json(),
     typescript({
       tsconfigOverride: {
         comppilerOptions: {
@@ -23,6 +23,6 @@ export default {
           moduleResolution: 'node'
         }
       }
-    })
+    }),
   ]
 }
